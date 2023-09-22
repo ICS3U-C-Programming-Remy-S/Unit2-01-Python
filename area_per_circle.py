86ba5751cd40e3a1c2db41327fb1cec53dfdaaaf
Remy Skelton <remy.skelton@stu.ocsb.ca>
2023-09-22 16:19:02 +0000

    Done area_per_circle.py

---
+++ area_per_circle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# Created by: Remy Skelton
+# Date: Sep. 22,2023
+# This program calculates the area and circumference of a circle
+
+
+import math
+
+
+def main():
+    print("For a circle with a radius of 17cm:")
+    print()
+    print("The area is: {}cm^2".format(math.pi * (17**2)))
+    print("The circumference is: {}cm".format(2 * math.pi * 12.329))
+
+
+if __name__ == "__main__":
+    main()
